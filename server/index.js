cc110495bb608b6b7de53bcbcf58d67795c90b6b
Nikhil <nikhil.4002.50.82@gmail.com>
2025-07-30 13:49:18 +0530

    updated server to use pg instead of ORM's

--- server/index.js
+++ server/index.js
@@ -1,42 +1,69 @@
 import express from "express";
 import cors from "cors";
-import { Sequelize, DataTypes } from "sequelize";
 import dotenv from "dotenv";
+import pkg from "pg";
 
-dotenv.config()
-
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: "localhost",
-  dialect: "postgres",
-  logging: false,
-});
-
-const Profile = sequelize.define("Profile", {
-  name: DataTypes.STRING,
-  url: DataTypes.STRING,
-  about: DataTypes.TEXT,
-  bio: DataTypes.TEXT,
-  location: DataTypes.STRING,
-  followerCount: DataTypes.STRING,
-  connectionCount: DataTypes.STRING,
-});
+const { Pool } = pkg;
+dotenv.config();
 
 const app = express();
+const port = 5000;
+
 app.use(cors());
 app.use(express.json());
 
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+const createTable = async () => {
+  const query = `
+    CREATE TABLE IF NOT EXISTS profiles (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR,
+      url VARCHAR,
+      about TEXT,
+      bio TEXT,
+      location VARCHAR,
+      followerCount VARCHAR,
+      connectionCount VARCHAR,
+      created_at TIMESTAMP DEFAULT NOW()
+    );
+  `;
+  await pool.query(query);
+};
+
 app.post("/api/profiles", async (req, res) => {
+  const { name, url, about, bio, location, followerCount, connectionCount } =
+    req.body;
   try {
-    const profile = await Profile.create(req.body);
-    res.json(profile);
+    const query = `
+  INSERT INTO profiles (name, url, about, bio, location, followerCount, connectionCount)
+  VALUES ($1, $2, $3, $4, $5, $6, $7)
+  RETURNING *;
+`;
+    const values = [
+      name,
+      url,
+      about,
+      bio,
+      location,
+      followerCount,
+      connectionCount,
+    ];
+    const result = await pool.query(query, values);
+    res.json(result.rows[0]);
   } catch (err) {
     console.error("Error saving profile:", err.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-sequelize.sync().then(() => {
-  app.listen(5000, () => {
-    console.log("Server running on http://localhost:5000");
-  });
+app.listen(port, async () => {
+  await createTable();
+  console.log(`Server running on port ${port}.`);
 });
